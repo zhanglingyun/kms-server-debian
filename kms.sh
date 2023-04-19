7250b38342e36081e85dd9474897df380537eb11
zhanglingyun <zhangshaofeng308@outlook.com>
2023-04-19 13:05:22 +0800

    correct
    correct command

--- kms.sh
+++ kms.sh
@@ -2,10 +2,10 @@
 apt-get update && apt-get upgrade -y
 apt-get install git wget curl gcc build-essential debhelper -y
 mkdir ~/vlmcsd-build
-cd ~/vlmcld-build
+cd ~/vlmcsd-build
 git clone https://github.com/Wind4/vlmcsd 
 cd ~/vlmcsd-build/vlmcsd
-git submodele update --init debian
+git submodule update --init debian
 dpkg-buildpackage -rfakeroot -D -us -uc
 cd ~/vlmcsd-build
 dpkg -i ./vlmcsd_1113_and64.deb
